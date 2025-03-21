9057ed4a69debb90deece6878d6579786aff84a7
JuanfranV <juanfranmendoza243@gmail.com>
2025-03-21 16:07:10 -0600

    Se casi de acaba agregar producto

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList <String> productos = new ArrayList();
+        ArrayList<String> productos = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
         String accion;
@@ -15,35 +15,37 @@ public class Main {
         System.out.println("* LISTA DE PRODUCTOS");
         System.out.println("* ACTUALIZAR CANTIDAD DE PRODUCTO");
         System.out.println("* ELIMINAR PRODUCTO");
-        accion = (scanner.nextLine()).toUpperCase();
+        accion = scanner.nextLine().toUpperCase();
 
-        if (accion.equals("AGREGAR PRODUCTO")){
+        if (accion.equals("AGREGAR PRODUCTO")) {
 
             String siONo;
             String nuevoProducto;
 
-            System.out.println("¿Comó se llama el nuevo producto?");
-            nuevoProducto = scanner.nextLine();
+            do {
+                System.out.println("¿Comó se llama el nuevo producto?");
 
-            System.out.println("¿Desea agregar " + nuevoProducto + " a la lista?");
+                nuevoProducto = scanner.nextLine();
 
-            System.out.println("NO");
-            System.out.println("SI");
-            siONo = (scanner.next()).toUpperCase();
+                System.out.println("¿Desea agregar " + nuevoProducto + " a la lista?");
 
+                System.out.println("NO");
+                System.out.println("SI");
+                siONo = scanner.next().toUpperCase();
+                scanner.nextLine();
+
+            } while (siONo.equals("NO"));
             if (siONo.equals("SI")) {
                 System.out.println("Se agregó a la lista");
                 productos.add(nuevoProducto);
             }
-            if (siONo.equals("NO")) {
-                System.out.println("No se agregó el producto");
-            }
-        }
+        } else {
+            if (accion.equals("LISTA DE PRODUCTOS")) {
+                System.out.println("La lista de productos es: ");
+                for (String producto : productos) {
+                    System.out.println(producto);
+                }
 
-        if (accion.equals("LISTA DE PODUCTOS")){
-            System.out.println("La lista de productos es: ");
-            for (String producto : productos){
-                System.out.println(producto);
             }
         }
     }
