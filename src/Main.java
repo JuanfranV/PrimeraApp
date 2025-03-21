cb4dcbbc1584352d20d59389ef4920d331e3db64
JuanfranV <juanfranmendoza243@gmail.com>
2025-03-21 11:47:15 -0600

    Se "acabó" agregar producto

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,50 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
 
-
+        ArrayList <String> productos = new ArrayList();
         Scanner scanner = new Scanner(System.in);
+
+        String accion;
 
         System.out.println("Hola ¿Qué deseas hacer?");
         System.out.println("* AGREGAR PRODUCTO");
         System.out.println("* LISTA DE PRODUCTOS");
         System.out.println("* ACTUALIZAR CANTIDAD DE PRODUCTO");
         System.out.println("* ELIMINAR PRODUCTO");
-        String accion;
         accion = (scanner.nextLine()).toUpperCase();
 
         if (accion.equals("AGREGAR PRODUCTO")){
-            System.out.println("¿Comó se llama el nuevo producto?");
+
+            String siONo;
             String nuevoProducto;
-            nuevoProducto = scanner.next();
+
+            System.out.println("¿Comó se llama el nuevo producto?");
+            nuevoProducto = scanner.nextLine();
 
             System.out.println("¿Desea agregar " + nuevoProducto + " a la lista?");
+
             System.out.println("NO");
             System.out.println("SI");
-            String siONo;
             siONo = (scanner.next()).toUpperCase();
-            if (siONo.equals("SI")){
+
+            if (siONo.equals("SI")) {
                 System.out.println("Se agregó a la lista");
-
-            }else {
-                System.out.println("no ahadfyd f er");
+                productos.add(nuevoProducto);
             }
-
+            if (siONo.equals("NO")) {
+                System.out.println("No se agregó el producto");
+            }
         }
 
-
+        if (accion.equals("LISTA DE PODUCTOS")){
+            System.out.println("La lista de productos es: ");
+            for (String producto : productos){
+                System.out.println(producto);
+            }
+        }
     }
 }
