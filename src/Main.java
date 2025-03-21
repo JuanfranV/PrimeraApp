0a5da3f38c9a08574ab4e74d843f4e8dace3cc62
JuanfranV <juanfranmendoza243@gmail.com>
2025-03-20 19:22:09 -0600

    inicio

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hola ¿Qué deseas hacer?");
+        String accion;
+        accion = (scanner.next()).toUpperCase();
+
+        System.out.println(accion + "sastre");
+
+
+    }
+}
