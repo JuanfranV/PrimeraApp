861565f8878eac15e206ea96e1e7ee3b034fde54
JuanfranV <juanfranmendoza243@gmail.com>
2025-03-21 20:39:53 -0600

    se terminó eliminar producto solo falta el uso de constante

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         String cantidadProducto;
         String nuevaCantidad;
         int numCambioCantidad;
-
+        int eliminarProducto;
         do {
             System.out.println("----------------------------");
             System.out.println("Hola ¿Qué deseas hacer?");
@@ -26,7 +26,7 @@ public class Main {
             System.out.println("* Salir");
             accion = scanner.nextLine().toUpperCase();
 
-            if (accion.equals("SALIR")){
+            if (accion.equals("SALIR")) {
                 break;
             }
 
@@ -58,12 +58,12 @@ public class Main {
                 listaCantidadPoducto.add("**Cantidad**");
 
                 System.out.println("La lista de productos es: ");
-                for (int i = 0; i < listaCantidadPoducto.size(); i++){
+                for (int i = 0; i < listaCantidadPoducto.size(); i++) {
                     System.out.println((i + 1) + ". " + "x" + listaCantidadPoducto.get(i) + " " + listaProductos.get(i));
                 }
             }
 
-            if (accion.equals("ACTUALIZAR CANTIDAD DE PRODUCTO")){
+            if (accion.equals("ACTUALIZAR CANTIDAD DE PRODUCTO")) {
                 System.out.println("A que producto desea cambiar la cantidad de producto. (1,2...)");
                 numCambioCantidad = scanner.nextInt();
 
@@ -73,17 +73,16 @@ public class Main {
                 listaCantidadPoducto.set((numCambioCantidad - 1), nuevaCantidad);
             }
 
-            if (accion.equals("ELIMINAR PRODUCTO")){
-                System.out.println("A que producto desea cambiar la cantidad de producto. (1,2...)");
-                numCambioCantidad = scanner.nextInt();
+            if (accion.equals("ELIMINAR PRODUCTO")) {
+                System.out.println("¿Qué producto desea eliminar? (1,2...)");
+                eliminarProducto = scanner.nextInt();
 
-                System.out.println("¿Cuál es la nueva cantidad?");
-                nuevaCantidad = scanner.next();
-
-                listaCantidadPoducto.set((numCambioCantidad - 1), nuevaCantidad);
+                listaCantidadPoducto.remove(eliminarProducto - 1);
+                listaProductos.remove(eliminarProducto - 1);
             }
 
-        }while (true);
+
+        } while (true);
 
     }
 }
